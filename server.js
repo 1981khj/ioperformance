508109712788b84f1ece437daddc247a955774b5
Hyeongjin, Kim <1981_khj@naver.com>
2012-03-26 09:18:04 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+var port = process.env.PORT;
+var express = require('express');
+var app = express.createServer();
+var fs = require('fs');
+var util = require('util');
+
+app.configure(function() {
+  app.set('views', __dirname + '/views');  
+  app.set('view engine', 'jade');  
+  app.register('.html', require('jade'));
+  app.set("view options", {layout: false});
+  app.use(express.favicon());
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.methodOverride());  
+  app.use(app.router);
+  app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+});
+
+app.configure('production', function(){
+  app.use(express.errorHandler()); 
+});
+
+app.get('/', function(req, res){
+    console.log('User-Agent: ' + req.headers['user-agent']);
+    var useragent = req.headers['user-agent'];
+    res.render('index', {
+    title: 'Express',
+    useragent: useragent
+  });
+});
+
+if (!module.parent) {
+  app.listen(port);
+  console.log('Server is Running! listening on port '+port);
+}
