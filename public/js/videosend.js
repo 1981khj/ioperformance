112be9c9edc829113fb3e4f3166a0747309364ac
Hyeongjin, Kim <1981_khj@naver.com>
2012-03-30 14:27:28 +0000

    mod

--- public/js/videosend.js
+++ public/js/videosend.js
@@ -7,20 +7,21 @@
     };
     
     //for dev
-    //var video = io.connect('http://ioperformance.hjkim.c9.io/video', options);
+    //var videosend = io.connect('http://ioperformance.hjkim.c9.io/video', options);
     
     //for deploy
-    var video = io.connect('http://ioperformance.herokuapp.com/video', options);    
+    var videosend = io.connect('http://ioperformance.herokuapp.com/video', options);
+    
     
     var conType = null;
     //처음 소켓 접속을 시도 하는 중인 경우
-    video.on('connecting', function(connectionType){        
+    videosend.on('connecting', function(connectionType){        
         status_update("Connecting connectionType: "+connectionType);
         conType = connectionType;
     });
     
     //처음 소켓 접속이 이루어진 경우
-    video.on('connect', function(){
+    videosend.on('connect', function(){
         status_update("Connected");
         $("#info").empty();
         console.log(video);
@@ -29,37 +30,38 @@
         info_update("connection Type: "+conType);
     });
     
-    video.on('connect_failed', function(){
+    videosend.on('connect_failed', function(){
         status_update("Connect Failed");        
     });
 
     //소켓 접속을 잃은 경우
-    video.on('disconnect', function(){
+    videosend.on('disconnect', function(){
         status_update("Disconnected");
     });
     
-    video.on('close', function (){
+    videosend.on('close', function (){
         status_update("Socket close");
     });
 
     //소켓 접속을 잃은 후 재 접속 시도
-    video.on('reconnecting', function( reconnetionDelay, reconnectionAttempts ) {
+    videosend.on('reconnecting', function( reconnetionDelay, reconnectionAttempts ) {
         status_update("Reconnecting in " + reconnetionDelay + " seconds"+" / attempts: "+reconnectionAttempts);        
     });
 
     //소켓 재접속이 성공적으로 이루 어진 경우
-    video.on('reconnect', function(transport_type, reconnectionAttempts){
+    videosend.on('reconnect', function(transport_type, reconnectionAttempts){
         status_update("Reconnected"+" transport_type: "+transport_type+" / attempts: "+reconnectionAttempts);
     });
 
     //소켓 재접속이 실패한 경우
-    video.on('reconnect_failed', function(){
+    videosend.on('reconnect_failed', function(){
         status_update("Reconnect Failed");
     });
 
 
     $("#start").bind("click",function(){
-        startTimer();        
+        startTimer();
+        //video.emit('receiveImg',"aa");
     });
 
     $("#stop").bind("click",function(){
@@ -67,16 +69,16 @@
     });
     
     //////////////////////////////
-    var elVideo  = $("#video")[0];
+    var video  = $("#video")[0];
     var canvas = $("#canvas")[0];
     var ctx = canvas.getContext('2d');
 	var intervalTimer=null;
     
     
     if(navigator.getUserMedia) {
-        navigator.getUserMedia('elVideo', successCallback, errorCallback);
+        navigator.getUserMedia('video', successCallback, errorCallback);
         function successCallback( stream ) {
-            elVideo.src = stream;
+            video.src = stream;
         }
         function errorCallback( error ) {
             alert("현재 브라우저는 카메라를 지원하지 못합니다.");
